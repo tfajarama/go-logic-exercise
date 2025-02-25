67aa82553c09289058c13cf5e50d3445dff1877e
Taufan Fajarama P.R. <taufan.fajarama@amartha.com>
2025-02-26 06:07:09 +0700

    refactor: matrix utils update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/tfajarama/go-logic-exercise
 
 go 1.24.0
 
-require github.com/tfajarama/go-print-slice v1.3.1
+require github.com/tfajarama/go-print-slice v1.3.2
--- utils/matrix_utils.go
+++ utils/matrix_utils.go
@@ -8,6 +8,14 @@ func InitMatrix(num int) (result [][]int) {
 	return result
 }
 
+func InitRectangleMatrix(rows int, cols int) (result [][]int) {
+	result = make([][]int, rows)
+	for i := 0; i < len(result); i++ {
+		result[i] = make([]int, cols)
+	}
+	return result
+}
+
 func InitStrMatrix(num int) (result [][]string) {
 	result = make([][]string, num)
 	for i := range result {
